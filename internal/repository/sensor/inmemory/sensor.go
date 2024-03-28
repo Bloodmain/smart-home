1f2aa6010927799f59a8681673d82f21fec8c85f
bloodmain <krytoipohtalon@mail.ru>
2024-03-28 21:13:33 +0300

    Tests passed

--- internal/repository/sensor/inmemory/sensor.go
+++ internal/repository/sensor/inmemory/sensor.go
@@ -4,34 +4,90 @@ import (
 	"context"
 	"errors"
 	"homework/internal/domain"
+	"sync"
+	"time"
 )
 
-var ErrSensorNotFound = errors.New("sensor not found")
+var (
+	ErrSensorNotFound   = errors.New("sensor not found")
+	ErrNilSensorPointer = errors.New("nil sensor is provided")
+)
 
 type SensorRepository struct {
-	// TODO добавьте реализацию
+	storage []*domain.Sensor
+	m       sync.RWMutex
 }
 
 func NewSensorRepository() *SensorRepository {
-	return &SensorRepository{}
+	return &SensorRepository{storage: []*domain.Sensor{}, m: sync.RWMutex{}}
 }
 
 func (r *SensorRepository) SaveSensor(ctx context.Context, sensor *domain.Sensor) error {
-	// TODO добавьте реализацию
-	return nil
+	if sensor == nil {
+		return ErrNilSensorPointer
+	}
+	sensor.RegisteredAt = time.Now()
+	r.m.Lock()
+	r.storage = append(r.storage, sensor)
+	r.m.Unlock()
+	return ctx.Err()
 }
 
 func (r *SensorRepository) GetSensors(ctx context.Context) ([]domain.Sensor, error) {
-	// TODO добавьте реализацию
-	return nil, nil
+	done := make(chan struct{})
+	sensors := make([]domain.Sensor, 0, len(r.storage))
+
+	go func() {
+		r.m.RLock()
+		for _, v := range r.storage {
+			sensors = append(sensors, *v)
+		}
+		r.m.RUnlock()
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case <-done:
+		return sensors, nil
+	}
 }
 
 func (r *SensorRepository) GetSensorByID(ctx context.Context, id int64) (*domain.Sensor, error) {
-	// TODO добавьте реализацию
-	return nil, nil
+	return r.getSensorBy(ctx, func(sensor *domain.Sensor) bool {
+		return sensor.ID == id
+	})
 }
 
 func (r *SensorRepository) GetSensorBySerialNumber(ctx context.Context, sn string) (*domain.Sensor, error) {
-	// TODO добавьте реализацию
-	return nil, ErrSensorNotFound
+	return r.getSensorBy(ctx, func(sensor *domain.Sensor) bool {
+		return sensor.SerialNumber == sn
+	})
+}
+
+func (r *SensorRepository) getSensorBy(ctx context.Context, p func(sensor *domain.Sensor) bool) (*domain.Sensor, error) {
+	done := make(chan struct{})
+	found := make(chan *domain.Sensor)
+
+	go func() {
+		r.m.RLock()
+		for _, v := range r.storage {
+			if p(v) {
+				found <- v
+				break
+			}
+		}
+		r.m.RUnlock()
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case s := <-found:
+		return s, nil
+	case <-done:
+		return nil, ErrSensorNotFound
+	}
 }
