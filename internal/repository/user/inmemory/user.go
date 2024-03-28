5c52c16e71bf2dade58eab82723e66a52d679c01
bloodmain <krytoipohtalon@mail.ru>
2024-03-28 21:13:33 +0300

    Tests passed

--- internal/repository/user/inmemory/user.go
+++ internal/repository/user/inmemory/user.go
@@ -4,24 +4,55 @@ import (
 	"context"
 	"errors"
 	"homework/internal/domain"
+	"sync"
 )
 
-var ErrUserNotFound = errors.New("user not found")
+var (
+	ErrUserNotFound   = errors.New("user not found")
+	ErrNilUserPointer = errors.New("nil user is provided")
+)
 
 type UserRepository struct {
-	// TODO добавьте реализацию
+	storage []*domain.User
+	m       sync.RWMutex
 }
 
 func NewUserRepository() *UserRepository {
-	return &UserRepository{}
+	return &UserRepository{storage: []*domain.User{}, m: sync.RWMutex{}}
 }
 
 func (r *UserRepository) SaveUser(ctx context.Context, user *domain.User) error {
-	// TODO добавьте реализацию
-	return nil
+	if user == nil {
+		return ErrNilUserPointer
+	}
+	r.m.Lock()
+	r.storage = append(r.storage, user)
+	r.m.Unlock()
+	return ctx.Err()
 }
 
 func (r *UserRepository) GetUserByID(ctx context.Context, id int64) (*domain.User, error) {
-	// TODO добавьте реализацию
-	return nil, nil
+	done := make(chan struct{})
+	found := make(chan *domain.User)
+
+	go func() {
+		r.m.RLock()
+		for _, v := range r.storage {
+			if v.ID == id {
+				found <- v
+				break
+			}
+		}
+		r.m.RUnlock()
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case s := <-found:
+		return s, nil
+	case <-done:
+		return nil, ErrUserNotFound
+	}
 }
