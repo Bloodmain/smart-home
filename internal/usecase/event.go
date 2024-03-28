b56ca328cce05eda26e003e9029d6040aebcb2f2
bloodmain <krytoipohtalon@mail.ru>
2024-03-28 21:13:33 +0300

    Tests passed

--- internal/usecase/event.go
+++ internal/usecase/event.go
@@ -3,38 +3,18 @@ package usecase
 import (
 	"context"
 	"homework/internal/domain"
-	"time"
 )
 
 type Event struct {
-	er EventRepository
-	sr SensorRepository
 	eventRepository  EventRepository
 	sensorRepository SensorRepository
 }
 
 func NewEvent(er EventRepository, sr SensorRepository) *Event {
-	return &Event{er: er, sr: sr}
 	return &Event{eventRepository: er, sensorRepository: sr}
 }
 
 func (e *Event) ReceiveEvent(ctx context.Context, event *domain.Event) error {
-	if event.Timestamp.IsZero() {
-		return ErrInvalidEventTimestamp
-	}
-	s, err := e.sr.GetSensorBySerialNumber(ctx, event.SensorSerialNumber)
-	if err != nil {
-		return err
-	}
-
-	event.SensorID = s.ID
-	s.LastActivity = event.Timestamp
-	s.CurrentState = event.Payload
-
-	if err = e.er.SaveEvent(ctx, event); err != nil {
-		return err
-	}
-	return e.sr.SaveSensor(ctx, s)
 	if event.Timestamp.IsZero() {
 		return ErrInvalidEventTimestamp
 	}
