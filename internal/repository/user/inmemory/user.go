ff57e5e9b2f1ac560b419f5c5ca1f1b54bc77966
bloodmain <krytoipohtalon@mail.ru>
2024-03-29 18:59:53 +0300

    not saving same sensors/users

--- internal/repository/user/inmemory/user.go
+++ internal/repository/user/inmemory/user.go
@@ -12,13 +12,15 @@ var (
 	ErrNilUserPointer = errors.New("nil user is provided")
 )
 
+type UserID int64
+
 type UserRepository struct {
-	storage []*domain.User
+	storage map[UserID]*domain.User
 	m       sync.RWMutex
 }
 
 func NewUserRepository() *UserRepository {
-	return &UserRepository{storage: []*domain.User{}, m: sync.RWMutex{}}
+	return &UserRepository{storage: map[UserID]*domain.User{}, m: sync.RWMutex{}}
 }
 
 func (r *UserRepository) SaveUser(ctx context.Context, user *domain.User) error {
@@ -26,29 +28,20 @@ func (r *UserRepository) SaveUser(ctx context.Context, user *domain.User) error 
 		return ErrNilUserPointer
 	}
 	r.m.Lock()
-	r.storage = append(r.storage, user)
+	r.storage[UserID(user.ID)] = user
 	r.m.Unlock()
 	return ctx.Err()
 }
 
 func (r *UserRepository) GetUserByID(ctx context.Context, id int64) (*domain.User, error) {
 	r.m.RLock()
-	for _, v := range r.storage {
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		default:
-			if v.ID == id {
-				return v, ctx.Err()
-			}
-		}
-	}
+	user, has := r.storage[UserID(id)]
 	r.m.RUnlock()
-
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	default:
+	if !has {
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
 		return nil, ErrUserNotFound
 	}
+	return user, ctx.Err()
 }
