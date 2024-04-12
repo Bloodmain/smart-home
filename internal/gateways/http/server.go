0788591169ce5bd6ad332e7fa16d762ad972c942
bloodmain <krytoipohtalon@mail.ru>
2024-04-12 03:13:59 +0300

    hw8

--- internal/gateways/http/server.go
+++ internal/gateways/http/server.go
@@ -14,6 +14,11 @@ type Server struct {
 	router *gin.Engine
 }
 
+const (
+	DefaultPort = 8080
+	DefaultHost = "localhost"
+)
+
 type UseCases struct {
 	Event  *usecase.Event
 	Sensor *usecase.Sensor
@@ -24,7 +29,7 @@ func NewServer(useCases UseCases, options ...func(*Server)) *Server {
 	r := gin.Default()
 	setupRouter(r, useCases, NewWebSocketHandler(useCases))
 
-	s := &Server{router: r, host: "localhost", port: 8080}
+	s := &Server{router: r, host: DefaultHost, port: DefaultPort}
 	for _, o := range options {
 		o(s)
 	}
