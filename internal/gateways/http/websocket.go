8a1d3c672e4db6a8aa21d89df2049103c7314316
bloodmain <krytoipohtalon@mail.ru>
2024-04-18 22:01:28 +0300

    hw9

--- internal/gateways/http/websocket.go
+++ internal/gateways/http/websocket.go
@@ -1,25 +1,94 @@
 package http
 
 import (
+	"encoding/json"
+	"errors"
+	"homework/internal/domain"
+	"sync"
+	"time"
+
 	"github.com/gin-gonic/gin"
+
+	"nhooyr.io/websocket"
 )
 
 type WebSocketHandler struct {
 	useCases UseCases
+
+	connections map[*websocket.Conn]struct{}
+	m           sync.Mutex
 }
 
 func NewWebSocketHandler(useCases UseCases) *WebSocketHandler {
 	return &WebSocketHandler{
-		useCases: useCases,
+		useCases:    useCases,
+		connections: make(map[*websocket.Conn]struct{}),
+		m:           sync.Mutex{},
 	}
 }
 
-func (h *WebSocketHandler) Handle(c *gin.Context, id int64) error {
-	// TODO implement me
+func (h *WebSocketHandler) Handle(ctx *gin.Context, id int64) error {
+	_, err := h.useCases.Sensor.GetSensorByID(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	conn, err := websocket.Accept(ctx.Writer, ctx.Request, nil)
+	if err != nil {
+		return err
+	}
+
+	h.m.Lock()
+	h.connections[conn] = struct{}{}
+	h.m.Unlock()
+
+	go func() {
+		c := conn.CloseRead(ctx)
+		t := time.NewTicker(time.Second * 2)
+		defer t.Stop()
+
+		lastEvent := domain.Event{}
+		for {
+			select {
+			case <-c.Done():
+				h.closeConn(conn, websocket.StatusNormalClosure, c.Err().Error())
+				return
+			case <-t.C:
+				event, err := h.useCases.Event.GetLastEventBySensorID(c, id)
+				if err != nil {
+					h.closeConn(conn, websocket.StatusInternalError, err.Error())
+					return
+				}
+				if lastEvent != *event {
+					lastEvent = *event
+					js, _ := json.Marshal(event)
+					err := conn.Write(c, websocket.MessageText, js)
+					if err != nil {
+						h.closeConn(conn, websocket.StatusInternalError, err.Error())
+						return
+					}
+				}
+			}
+		}
+	}()
+
 	return nil
 }
 
+func (h *WebSocketHandler) closeConn(conn *websocket.Conn, code websocket.StatusCode, reason string) {
+	conn.Close(code, reason)
+
+	h.m.Lock()
+	delete(h.connections, conn)
+	h.m.Unlock()
+}
+
 func (h *WebSocketHandler) Shutdown() error {
-	// TODO implement me
-	return nil
+	h.m.Lock()
+	var e []error
+	for c := range h.connections {
+		e = append(e, c.Close(websocket.StatusNormalClosure, "server shutting down"))
+	}
+	h.m.Unlock()
+	return errors.Join(e...)
 }
