722f890c1362f73cb436823618fe65815bce80e8
bloodmain <krytoipohtalon@mail.ru>
2024-04-26 03:16:08 +0300

    HW10 table method

--- internal/gateways/http/router_test.go
+++ internal/gateways/http/router_test.go
@@ -3,8 +3,6 @@ package http
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 	eventRepository "homework/internal/repository/event/inmemory"
 	sensorRepository "homework/internal/repository/sensor/inmemory"
 	userRepository "homework/internal/repository/user/inmemory"
@@ -15,6 +13,9 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 var (
