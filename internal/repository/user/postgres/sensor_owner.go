596967b9fd9a7ae7fd444f7a6f051ca7ac7ca160
bloodmain <krytoipohtalon@mail.ru>
2024-05-15 21:32:48 +0300

    HW12

--- internal/repository/user/postgres/sensor_owner.go
+++ internal/repository/user/postgres/sensor_owner.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 	"homework/internal/domain"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -17,12 +18,34 @@ func NewSensorOwnerRepository(pool *pgxpool.Pool) *SensorOwnerRepository {
 	}
 }
 
+const saveSensorOwnerQuery = `insert into db.public.sensors_users (sensor_id, user_id) values ($1, $2);`
+
 func (r *SensorOwnerRepository) SaveSensorOwner(ctx context.Context, sensorOwner domain.SensorOwner) error {
-	// TODO добавьте реализацию
-	return nil
+	_, err := r.pool.Exec(ctx, saveSensorOwnerQuery, sensorOwner.SensorID, sensorOwner.UserID)
+	if err != nil {
+		return err
+	}
+	return ctx.Err()
 }
 
+const getSensorsByUserId = `select sensor_id, user_id from db.public.sensors_users where user_id = $1;`
+
 func (r *SensorOwnerRepository) GetSensorsByUserID(ctx context.Context, userID int64) ([]domain.SensorOwner, error) {
-	// TODO добавьте реализацию
-	return nil, nil
+	rows, err := r.pool.Query(ctx, getSensorsByUserId, userID)
+	if err != nil {
+		return nil, fmt.Errorf("can't select sensors by user id %d %w", userID, err)
+	}
+	defer rows.Close()
+
+	sensors := make([]domain.SensorOwner, 0)
+	for rows.Next() {
+		sensor := domain.SensorOwner{}
+		if err := rows.Scan(&sensor.SensorID, &sensor.UserID); err != nil {
+			return nil, fmt.Errorf("can't scan sensor owner: %w", err)
+		}
+
+		sensors = append(sensors, sensor)
+	}
+
+	return sensors, ctx.Err()
 }
